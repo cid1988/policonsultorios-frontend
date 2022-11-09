97e9edb033dbb51980b7d673042e517ae3e5b8e7
Pablo <perellopablo1988@gmail.com>
2022-11-09 00:47:49 -0300

    Add interceptor

---
+++ src/app/shared-modules/interceptors/token-interceptor.service.ts
@@ -0,0 +1,39 @@
+import { HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { CookieService } from 'ngx-cookie';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { LoginService } from 'src/app/modules/login/services/login.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TokenInterceptorService implements HttpInterceptor {
+
+  constructor(private cookieService: CookieService, private router: Router, private loginService: LoginService){}
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const idToken = this.cookieService.get("token");
+
+    let request = req;
+    if (idToken) {
+      request = req.clone({
+        setHeaders: {
+          authorization: `Bearer ${ idToken }`
+        }
+      });
+    }else{
+      this.router.navigateByUrl('/login');
+    }
+
+    return next.handle(request).pipe(
+      catchError((err: HttpErrorResponse) => {
+        if (err.status === 401) {
+          this.router.navigateByUrl('/login');
+        }
+        return throwError( err );
+      })
+    );
+  }
+}
